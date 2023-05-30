2fead4a057b0e646ce67e1a2ed4cd09ced67332d
Deskardo <kalugin.aleksei.v@gmail.com>
2023-05-30 07:40:14 +0300

    4_1_semi_complete

---
+++ 4_1_seminar/4_2.py
@@ -0,0 +1,11 @@
+# Пользователь вводит текст(строка). Словом считается последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом пробелов. Определите, сколько различных слов содержится в этом тексте.
+
+text = "She sells sea shells on the sea shore The shellsthat she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells".split()
+text_pl = {" ",}
+print(text)
+
+for i in range(len(text)):
+    text_pl.add(text[i])
+    
+print(len(text_pl))
